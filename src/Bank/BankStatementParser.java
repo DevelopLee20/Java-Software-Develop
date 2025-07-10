4080cb9aaebc33e9a398e3f58de5ddf84199ff6b
LeeInGyu <Developlee20@gmail.com>
2025-07-10 18:14:56 +0900

    Refactor: 결합도 낮추기(2)

---
+++ src/Bank/BankStatementParser.java
@@ -0,0 +1,9 @@
+package Bank;
+
+import java.util.List;
+
+public interface BankStatementParser {
+    BankTransaction parseFrom(String line);
+
+    List<BankTransaction> parseLinesFrom(List<String> lines);
+}
---
+++ src/Bank/BankTransactionParser.java
@@ -0,0 +1,16 @@
+package Bank;
+
+// 논리 응집
+public class BankTransactionParser {
+    public BankTransaction parseFromCSV(final String line){
+        throw new UnsupportedOperationException();
+    }
+
+    public BankTransaction parseFromJSON(final String line){
+        throw new UnsupportedOperationException();
+    }
+
+    public BankTransaction parseFromXML(final String line){
+        throw new UnsupportedOperationException();
+    }
+}
