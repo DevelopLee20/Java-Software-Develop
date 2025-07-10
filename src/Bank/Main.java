3e9abccadde57d7f4565b05da1d86bc57c692ddf
LeeInGyu <Developlee20@gmail.com>
2025-07-10 18:13:37 +0900

    Refactor: 결합도 낮추기

--- src/Bank/Main.java
+++ src/Bank/Main.java
@@ -1,48 +1,35 @@
 package Bank;
 
-import Bank.BankStatementCSVParser;
-import Bank.BankTransaction;
-
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.time.Month;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        final BankStatementAnalyzer bankStatementAnalyzer = new BankStatementAnalyzer();
+        final BankStatementParser bankStatementParser = new BankStatementCSVParser();
+
+        bankStatementAnalyzer.analyze(args[0], bankStatementParser);
+
         final BankStatementCSVParser bankStatementCSVParser = new BankStatementCSVParser();
-
-        final String RESOURCES = "../src/"; // csv 파일 위치 지정
-//        final String fileName = "bank-date-simple.csv";
-        final String fileName = args[0];
-        final Path path = Paths.get(RESOURCES + fileName);
-        final List<String> lines = Files.readAllLines(path);
-        final List<BankTransaction> bankTransactions = bankStatementCSVParser.parseLinesFromCSV(lines);
-        final Month month = Month.JANUARY;
-
-        System.out.println("The total: " + calculateTotalAmount(bankTransactions));
-        System.out.println("Transactions: " + month + " " + selectInMonth(bankTransactions, month));
     }
 
-    public static double calculateTotalAmount(final List<BankTransaction> bankTransactions){
+    public static double calculateTotalAmount(final List<BankTransaction> bankTransactions) {
         double total = 0d;
 
-        for (BankTransaction bankTransaction : bankTransactions){
+        for (BankTransaction bankTransaction : bankTransactions) {
             total += bankTransaction.getAmount();
         }
 
         return total;
     }
 
-    public static List<BankTransaction> selectInMonth(final List<BankTransaction> bankTransactions, final Month month){
+    public static List<BankTransaction> selectInMonth(final List<BankTransaction> bankTransactions, final Month month) {
         final List<BankTransaction> bankTransactionsInMonth = new ArrayList<>();
 
-        for (BankTransaction bankTransaction : bankTransactions){
-            if(bankTransaction.getDate().getMonth() == month){
+        for (BankTransaction bankTransaction : bankTransactions) {
+            if (bankTransaction.getDate().getMonth() == month) {
                 bankTransactionsInMonth.add(bankTransaction);
             }
         }
